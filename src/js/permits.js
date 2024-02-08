08aaeebace4e9b87d9c6c2847657467014591b2a
dcodeeer <dcodeeer@proton.me>
2024-02-09 03:01:59 +0600

    09.02.2024 03:01

---
+++ src/js/permits.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = new Swiper('.slider-container', {
+    init: false,
+    loop: true,
+    speed: 500,
+    autoplay: {
+        delay: 2000,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 80,
+      depth: 200,
+      modifier: 1,
+      slideShadows: false,
+    },
+
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev',
+    },
+  });
+
+  slider.on('slideChange', (swiper) => {
+    document.querySelector('#current-slide').innerHTML = swiper.realIndex + 1;
+  });
+
+  slider.on('init', (swiper) => {
+    document.querySelector('#slides-count').innerHTML = swiper.slides.length;
+  });
+
+  slider.init();
+});
