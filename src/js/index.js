466e652a8498068d73da5dd397397f73a92fed36
dcodeeer <dcodeeer@proton.me>
2024-02-12 13:56:19 +0600

    12.02.2024 13:56

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
-  const swiper2 = new Swiper('.tabs', {
+  const swiper2 = new Swiper('#slider-tabs', {
     slidesPerView: "auto",
     navigation: {
       prevEl: '#tab-prev-btn',
@@ -35,4 +35,72 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     }
   });
+
+   const modalSlider = new Swiper('#modal-slider', {
+    init: false,
+    slidesPerView: "auto",
+    navigation: {
+      prevEl: '#modal-prev',
+      nextEl: '#modal-next'
+    },
+
+    breakpoints: {
+      1249: {
+        spaceBetween: 15,
+      },
+      1250: {
+        spaceBetween: 64,
+      },
+    }
+  });
+
+  // tab
+
+  const slides = document.querySelector('.tab-slides[data-id="1"]');
+  document.querySelector('#modal-slider .swiper-wrapper').innerHTML = slides.innerHTML;
+  modalSlider.init();
+
+  const tabOpenListener = (e) => {
+    document.querySelectorAll('.tab-title').forEach((tabTitle) => tabTitle.classList.remove('active'));
+    e.currentTarget.classList.add('active');
+
+    const id = e.currentTarget.getAttribute('data-id');
+
+    const slides = document.querySelector(".tab-slides[data-id='"+ id +"']");
+    console.log(slides);
+    document.querySelector('#modal-slider .swiper-wrapper').innerHTML = slides.innerHTML;
+    modalSlider.update();
+
+    const content = document.querySelector(".tab-content[data-id='"+ id +"']");
+
+    const currentVisible = document.querySelector(".tab-content.visible");
+    currentVisible.classList.remove('visible');
+    content.classList.add('visible');
+  };
+
+
+  const tabs = document.querySelectorAll('.tab-title');
+  tabs.forEach((tab) => tab.addEventListener('click', tabOpenListener));
+
+  // tab
+
+
+  // modal
+
+  const modalOpenListener = () => {
+    const body = document.querySelector('body');
+
+    const modal = document.querySelector('.modal-box');
+    modal.classList.add('modal-box-opened');
+    body.style.overflow = 'hidden';
+
+    document.getElementById('close-modal').addEventListener('click', () => {
+      modal.classList.remove('modal-box-opened')
+      body.style.overflow = 'visible';
+    });
+  };
+
+  const blocks = document.querySelectorAll('.slide-open-modal');
+  blocks.forEach(block => block.addEventListener('click', modalOpenListener))
+  // modal
 });
