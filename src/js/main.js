28258eca380b93615932cc99925d2500ab05e93c
dcoder <dcodeeer@proton.me>
2024-03-10 23:32:50 +0500

    10.03.2024 23:32

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
   mobileToggle.addEventListener('change', () => {
     if (mobileToggle.checked) {
       body.style.overflow = 'hidden';
-      footer.style.zIndex = '-1';
-      wrapper.style.zIndex = '-1';
     } else {
       body.style.overflow = 'auto';
-      footer.style.zIndex = '0';
-      wrapper.style.zIndex = '0';
     }
   });
 
